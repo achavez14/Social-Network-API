5408ab6fae99c3931ac15cb333745d41e39a4c36
Alexa Chavez <aguinada14@gmail.com>
2024-04-24 15:17:23 -0700

    Added thought model schema with reaction count virtual field

---
+++ Models/thought.js
@@ -0,0 +1,26 @@
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reaction.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports =  Thought;
