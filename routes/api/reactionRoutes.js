17b9f752a79d7e4dbb08d4b8389bea286686175b
Alexa Chavez <aguinada14@gmail.com>
2024-04-24 16:58:07 -0700

    Added CRUD operations in reactionRoutes.js file

---
+++ routes/api/reactionRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// Define CRUD operations for reactions
+// Get all reactions
+router.get('/', (req, res) => {
+});
+
+// Add a new reaction
+router.post('/', (req, res) => {
+});
+
+// Remove a reaction by ID
+router.delete('/:id', (req, res) => {
+});
+
+module.exports = router;
