d007db95e3c93667e823c6e1e0069f47adc5d39d
Alexa Chavez <aguinada14@gmail.com>
2024-04-24 17:05:19 -0700

    Added CRUD operations in friendRoutes.js file

---
+++ routes/api/friendRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// Define CRUD operations for friends
+// Get all friends
+router.get('/', (req, res) => {
+});
+
+// Add a new friend
+router.post('/', (req, res) => {
+});
+
+// Remove a friend by ID
+router.delete('/:id', (req, res) => {
+});
+
+module.exports = router;
