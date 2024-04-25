684a5fb7dfd9f35e7b6c38c59f5d7ed3f1fa0ef8
Alexa Chavez <aguinada14@gmail.com>
2024-04-24 20:13:19 -0700

    Updated server.js file

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ const express = require('express');
 const connectDB = require('./config/connection');
 const app = express();
 
+// Connect to MongoDB
 connectDB();
 
+// Middleware
 app.use(express.json());
 
-app.use('/api/notes', require('./routes/notes'));
+// Define Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Default Route
+app.get('*', (req, res) => {
+  res.status(404).send('Page not found');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
